9ef1c8ea5a374db9cb3857610e93f4161385f7a1
Zaid123-hub <zaidsayyed09876@gmail.com>
2026-08-30 16:09:54 +0530

    Time: 3 ms (18.04%) | Memory: 21.3 MB (15.37%) - LeetSync

--- 4410-count-integers-appearing-in-a-single-block/count-integers-appearing-in-a-single-block.cpp
+++ 4410-count-integers-appearing-in-a-single-block/count-integers-appearing-in-a-single-block.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int countSpecialIntegers(vector<int>& nums) {
-        int n=nums.size();
-        unordered_map<int,int> m;
-        unordered_set<int> notspecial;
-        for(int i=0;i<n;i++){
-            m[nums[i]]++;
+        int n = nums.size();
+        unordered_map<int,int>mp;
+        for(int x : nums){
+            mp[x]++;
         }
-        int count = 0;
-        for(int i=0;i<n;i++){
-            m[nums[i]]--;
-            if(i==n-1 || nums[i]!=nums[i+1]){
-             if(m[nums[i]]>0) {
-                notspecial.insert(nums[i]);
-             }
-            
+        int ans = 0;
+        vector<int>block(101,0);
+        block[nums[0]]++;
+        for(int i = 1;i<n;i++){
+            if(nums[i] != nums[i-1]){
+                block[nums[i]]++;
+            }
         }
+
+        for(int i = 1;i<=100;i++){
+            if(block[i] == 1){
+                ans ++;
+            }
         }
-        return m.size()-notspecial.size();
+
+        return ans;
     }
 };
