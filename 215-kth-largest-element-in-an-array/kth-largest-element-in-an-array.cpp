3d5128de4f87273137451e6d22156dcf6065a538
Zaid123-hub <zaidsayyed09876@gmail.com>
2026-09-03 00:43:40 +0530

    Time: 26 ms (80.03%) | Memory: 69.1 MB (91.55%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -4,9 +4,6 @@ public:
         int n = nums.size();
         int maxi = INT_MIN;
         sort(nums.begin(),nums.end());
-        for(int i=n-1;i>n-1-k;i--){
-            maxi = nums[i];
-        }
-        return maxi;
+        return nums[n-k];
     }
 };
