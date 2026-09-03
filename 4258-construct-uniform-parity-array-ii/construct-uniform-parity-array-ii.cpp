0157a79ad70e434c3ef8e4b680794087f51e6e4d
Zaid123-hub <zaidsayyed09876@gmail.com>
2026-09-03 20:56:04 +0530

    Time: 7 ms (51.05%) | Memory: 165.8 MB (87.43%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -9,11 +9,14 @@ public:
             if(nums1[i] % 2 != 0) all_even = false;
         }
         if (all_odd || all_even) return true;
-
-        sort(nums1.begin(),nums1.end());
-        bool odd = false;
-        // trying making all o
-        if(nums1[0] % 2 != 0) return true;
+        int mino = INT_MAX;
+        int mine = INT_MAX;
+        for(int i=0;i<n;i++){
+                if(nums1[i]%2==0) mine = min(mine,nums1[i]);
+                else mino = min(mino,nums1[i]);
+        }
+       
+        if(mino < mine) return true;
         else return false;
         // trying making all e
     }
