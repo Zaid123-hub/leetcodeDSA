78cfbd81bcab9fddee240547aeed30b5b8abc2a1
Zaid123-hub <zaidsayyed09876@gmail.com>
2026-09-03 20:51:55 +0530

    Time: 65 ms (23.56%) | Memory: 166 MB (31.94%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -13,13 +13,8 @@ public:
         sort(nums1.begin(),nums1.end());
         bool odd = false;
         // trying making all o
-        for(int i = 0;i<n;i++){
-            if(nums1[i] % 2 != 0) odd = true;
-            if(nums1[i] % 2 == 0){
-                if(odd) return true;
-                else break;
-            }
-        }
+        if(nums1[0] % 2 != 0) return true;
+        else return false;
         // trying making all e
         return false;
     }
