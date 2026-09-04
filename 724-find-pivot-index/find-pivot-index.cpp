313b61169c81e0ca8ecd69e75f0de4f6a54ffa55
Zaid123-hub <zaidsayyed09876@gmail.com>
2026-09-04 11:55:00 +0530

    Time: 0 ms (100.00%) | Memory: 37.1 MB (10.61%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -4,13 +4,13 @@ public:
         int n = nums.size();
         vector<int> pre(n);
         vector<int> suff(n);
-        pre[0] = nums[0];
+        pre[0] = 0;
         for(int i=1;i<n;i++){
-            pre[i] = pre[i-1]+nums[i];
+               pre[i] = pre[i-1]+nums[i-1];
         }
-        suff[n-1] = nums[n-1];
+        suff[n-1] = 0;
         for(int i=n-2;i>=0;i--){
-            suff[i] = suff[i+1]+nums[i];
+            suff[i] = suff[i+1]+nums[i+1];
         }
         for(int i=0;i<n;i++){
             if(pre[i]==suff[i]) return i;
