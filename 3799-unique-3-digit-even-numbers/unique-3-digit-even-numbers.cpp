12f4ddba38aafadaa8cb6b3afc30646597f80ef9
Zaid123-hub <zaidsayyed09876@gmail.com>
2026-09-11 22:58:06 +0530

    Time: 4 ms (79.08%) | Memory: 29.3 MB (80.99%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -3,19 +3,26 @@ public:
     int totalNumbers(vector<int>& digits) {
         int n = digits.size();
         int count = 0;
-        set<int> st;
+        unordered_map<int,int> mp;
         for(int i=0;i<n;i++){
-              if(digits[i]==0) continue;
-            for(int j=0;j<n;j++){
-                for(int k=0;k<n;k++){
-                   if(digits[k]%2!=0) continue;
-                   if(i == j || i == k || j == k) continue;
-                   int num = digits[i] * 100 + digits[j] * 10 + digits[k];
-                   st.insert(num);
-                }
-            }
-            
+            mp[digits[i]]++;
         }
-        return st.size();
+        for(int i=1;i<=9;i++){
+            if(mp[i]==0) continue;
+            mp[i]--;
+            for(int j=0;j<=9;j++){
+                    if(mp[j]==0) continue;
+                    mp[j]--;
+                for(int k=0;k<9;k+=2){
+                        if(mp[k]==0) continue;
+                    
+                        count++;
+                     
+                }
+                mp[j]++;
+            }
+            mp[i]++;
+        }
+        return count;
     }
 };
