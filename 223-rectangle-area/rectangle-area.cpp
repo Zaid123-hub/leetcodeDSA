85939fa51abb10f589db0f86f919863dcf119b18
Zaid123-hub <zaidsayyed09876@gmail.com>
2026-09-14 12:48:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (39.67%) - LeetSync

---
+++ 223-rectangle-area/rectangle-area.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        bool cArea = false;
+       if((ax1<bx2 && bx1<ax2) && (ay2>by1 && by2>ay1)){
+        cArea = true;
+       } 
+       int commonArea = 0;
+       int A1 = (ax2-ax1)*(ay2-ay1);
+       int A2 = (bx2-bx1)*(by2-by1);
+       if(cArea==true){
+        commonArea = (min(ax2, bx2) - max(ax1, bx1))*(min(ay2, by2) - max(ay1, by1));
+       }
+       int totalarea = A1+A2-commonArea;
+       return totalarea;
+    }
+};
